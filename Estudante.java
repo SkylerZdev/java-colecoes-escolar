0e58a96902edd880b97e4d711cea6118f5b5eb27
SkylerZdev <bennymaciel28@gmail.com>
2025-10-17 16:27:19 -0300

    Criacao das classes base: Estudante, Disciplina e Matricula
    Criacao das classes que serão utilizadas durante o desenvolvimento desse
    sistema, com seus respectivos getters e setters.

---
+++ Estudante.java
@@ -0,0 +1,16 @@
+public class Estudante {
+    private int id;
+    private String nome;
+
+    public Estudante(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+    
+    public int getId() {
+        return id;
+    }
+    public String getNome() {
+        return nome;
+    }
+}
