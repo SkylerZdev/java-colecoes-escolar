850a6ab0fb1040eec47333db3a73da9713464566
Eric Santos Moura Lima <ericsmoura01@gmail.com>
2025-10-18 16:08:34 -0300

    Update Disciplina.java
    Adicão dos métodos:
    - equals
    -hashCode 
    -toString 
    Objetivo: comparar objetos e mostrar melhor as informações

--- Disciplina.java
+++ Disciplina.java
@@ -6,12 +6,30 @@ public class Disciplina {
         this.codigo = codigo;
         this.nome = nome;
     }
-    
+
     public int getCodigo() {
         return codigo;
     }
+
     public String getNome() {
         return nome;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Disciplina)) return false;
+        Disciplina d = (Disciplina) o;
+        return this.codigo == d.codigo;
+    }
+
+    @Override
+    public int hashCode() {
+        return Integer.hashCode(codigo);
+    }
+
+    @Override
+    public String toString() {
+        return nome + " (" + codigo + ")";
+    }
 }
